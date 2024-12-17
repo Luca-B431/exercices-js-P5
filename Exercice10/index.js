2c005f8350814fa6c063cc2849f64945bc00e5cc
Luca Billat <lucabillatpro@gmail.com>
2024-12-17 11:57:22 +0100

    Exercices done

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,10 @@
 //votre code ici
+function printNumbers(n) {
+  let number = "";
+  for (let i = 1; i <= n; i++) {
+    number += i + " ";
+  }
+  return number.trim();
+}
 
-export default printNumbers
+export default printNumbers;
--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,13 @@
 // votre code ici
+function checkAge(age) {
+  if (age < 18) {
+    return "Vous êtes mineur.";
+  } else if (age >= 65) {
+    // Modifié pour inclure 65 dans les seniors
+    return "Vous êtes senior.";
+  } else {
+    return "Vous êtes majeur.";
+  }
+}
 
 export default checkAge;
